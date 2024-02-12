f7d73a55d5c2ad22568c80ed360cf9a6afb7de47
Jenby32 <kevinkozar2005@gmail.com>
2024-02-12 16:13:25 +0100

    added formula for male and female calorie calculation

--- index.php
+++ index.php
@@ -61,14 +61,27 @@
 </form>
 
 <?php
+
+/*
+Frauen: 655,1 + (9,6 x Körpergewicht in kg) + (1,8 x Körpergröße in cm) – (4,7 x Alter in Jahren)
+Männer: 66,47 + (13,7 x Körpergewicht in kg) + (5 x Körpergröße in cm) – (6,8 x Alter in Jahren)
+*/
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $gender = $_POST["selectbox_gender"];
         $weight = $_POST["text_weight"];
         $height = $_POST["text_height"];
+        $age = $_POST["text_age"];
         $activity = $_POST["selectbox_activity"];
         $sittingTime = $_POST["text_sitting"];
         $standingTime = $_POST["text_standing"];
 
-        echo $gender. " " .$weight. " " .$height." ".$activity." ".$sittingTime." ".$standingTime."";
+        $kalorienBedarf = 0;
+        if($gender == "male") {
+          $kalorienBedarf = 66.47 + (13.7*$weight) + (5*$height) - (6.8*$age);
+        } else {
+          $kalorienBedarf = 655.1 + (9.6*$weight) + (1.8*$height) - (4.7*$age);
+        }
+        echo $gender. " " .$weight. " " .$height." ".$activity." ".$sittingTime." ".$standingTime."<br/>";
+        echo "Der Standardkalorienbedarf für dieses Alter, diese Größe und dieses Körpergewicht beträgt: " .$kalorienBedarf. "kcal";
     }
 ?>
