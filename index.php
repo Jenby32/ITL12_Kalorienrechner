6cea6956f978486387079dba964d2cc88caac235
Jenby32 <kevinkozar2005@gmail.com>
2024-02-12 18:38:15 +0100

    added function to calculate PAL Average as described in the excercise

--- index.php
+++ index.php
@@ -56,11 +56,26 @@
   </div>
 
   <div class="form-group">
+    <label>Büro</label> <input type="text" class="form-control" name="text_office" placeholder="Büro - h">
+  </div>
+
+  <div class="form-group">
     <input type="submit" class="btn btn-primary" name="button_send" value="Senden">
   </div><small>Felder markiert mit * sind Pflichtfelder.</small>
 </form>
 
 <?php
+
+    function getPalAvg($factors, $sittingH, $officeH, $standingH) {
+        /*
+          Bsp: 4 h sitzend, 6 h Büro, 2 h stehend/gehend = 12 h Gesamt dh. 12 h Schlaf bleiben übrig.
+          Berechnung: 4*1,2 + 6*1,45+2*1,85+12*0,95 / 24 = 1,19 (Durchschnitt)
+        */
+        $activeHours = $sittingH + $standingH + $officeH;
+        $activeHoursPAL = $sittingH*$factors["only_sit"]+ $standingH*$factors["mainly_stand"]+$officeH*$factors["mainly_sit"];
+        $sleepTime = (24 - $activeHours)*$factors["sleep"];
+        return ($activeHoursPAL+$sleepTime) / 24;
+    }
 
     $activityPALFac = array(
       "only_sit"=> 1.2,
@@ -83,6 +98,7 @@ Männer: 66,47 + (13,7 x Körpergewicht in kg) + (5 x Körpergröße in cm) – 
         $activity = $_POST["selectbox_activity"];
         $sittingTime = $_POST["text_sitting"];
         $standingTime = $_POST["text_standing"];
+        $officeTime = $_POST["text_office"];
 
         $kalorienBedarf = 0;
         if($gender == "male") {
@@ -90,7 +106,11 @@ Männer: 66,47 + (13,7 x Körpergewicht in kg) + (5 x Körpergröße in cm) – 
         } else {
           $kalorienBedarf = 655.1 + (9.6*$weight) + (1.8*$height) - (4.7*$age);
         }
-        echo $gender. " " .$weight. " " .$height." ".$activity." ".$sittingTime." ".$standingTime."<br/>";
+
+        echo "Der Durchschnitt für den PAL Wert beträgt: " .getPalAvg($activityPALFac, $sittingTime, $officeTime, $standingTime) ."<br/>";
+
+
+        // echo $gender. " " .$weight. " " .$height." ".$activity." ".$sittingTime." ".$standingTime."";
         echo "Der Standardkalorienbedarf für dieses Alter, diese Größe und dieses Körpergewicht beträgt: " .$kalorienBedarf. "kcal";
     }
 ?>
