0c31da94ff99a5e528b72f5e3997e5549c249d3c
Jenby32 <kevinkozar2005@gmail.com>
2024-02-12 14:28:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<form>
+  <div class="form-group">
+    <label>Geschlecht</label> <select class="form-control" name="selectbox_gender">
+      <option value="male">
+        Männlich
+      </option>
+
+      <option value="female">
+        Weiblich
+      </option>
+    </select>
+  </div>
+
+  <div class="form-group">
+    <label>Alter</label> <input type="text" class="form-control" name="text_age" placeholder="Alter">
+  </div>
+
+  <div class="form-group">
+    <label>Gewicht</label> <input type="text" class="form-control" name="text_weight" placeholder="Gewicht - kg">
+  </div>
+
+  <div class="form-group">
+    <label>Größe</label> <input type="text" class="form-control" name="text_height" placeholder="Größe - cm">
+  </div>
+</form>
